ef95516e863d8d42f42342f977d094afdb68cfcf
Or Avner <or@vsecgroup.com>
2018-04-14 19:39:38 +0300

    added mem stage, fixed some bugs

--- HW1/sim_core.cpp
+++ HW1/sim_core.cpp
@@ -4,6 +4,8 @@
 #include "sim_api.h"
 
 SIM_coreState state;
+int32_t stage_dest_val[SIM_PIPELINE_DEPTH];
+bool last_read_mem_failed;
 
 #define STAGE_IF    0
 #define STAGE_ID    1
@@ -11,8 +13,9 @@ SIM_coreState state;
 #define STAGE_MEM   3
 #define STAGE_WB    4
 
-void reset_cmd(SIM_cmd* cmd) {
-  memset(cmd, 0, sizeof(SIM_cmd));
+void reset_stage(uint32_t stage) {
+  memset(&state.pipeStageState[stage], 0, sizeof(PipeStageState));
+  stage_dest_val[stage] = 0;
 }
 
 /*! SIM_CoreReset: Reset the processor core simulator machine to start new simulation
@@ -29,33 +32,58 @@ int SIM_CoreReset(void) {
     state.regFile[i] = 0;
   memset(&state, 0, sizeof(SIM_coreState));
   SIM_MemInstRead(state.pc, &state.pipeStageState[STAGE_IF].cmd);
+  last_read_mem_failed = false;
   return 0;
 }
 
 void DoIDStage() {
-    state.pipeStageState[STAGE_ID].src1Val = state.regFile[state.pipeStageState[STAGE_ID].cmd.src1];
-    if (state.pipeStageState[STAGE_ID].cmd.isSrc2Imm) {
-      state.pipeStageState[STAGE_ID].src2Val = state.pipeStageState[STAGE_ID].cmd.src2;
-    }
-    else {
-      state.pipeStageState[STAGE_ID].src2Val = state.regFile[state.pipeStageState[STAGE_ID].cmd.src2];
-    }
+  PipeStageState* current_stage = &state.pipeStageState[STAGE_ID];
+
+  current_stage->src1Val = state.regFile[current_stage->cmd.src1];
+  if (current_stage->cmd.isSrc2Imm) {
+    current_stage->src2Val = current_stage->cmd.src2;
+  }
+  else {
+    current_stage->src2Val = state.regFile[current_stage->cmd.src2];
+  }
+  
+  if (current_stage->cmd.opcode == CMD_STORE) {
+    stage_dest_val[STAGE_ID] = state.regFile[current_stage->cmd.dst];
+  }
 }
 
 void DoEXStage() {
   PipeStageState* current_stage = &state.pipeStageState[STAGE_EX];
   switch(current_stage->cmd.opcode) {
     case CMD_ADD:
-      current_stage->src1Val = current_stage->src1Val + current_stage->src2Val;
+      stage_dest_val[STAGE_EX] = current_stage->src1Val + current_stage->src2Val;
       break;
     case CMD_ADDI:
-      current_stage->src1Val = current_stage->src1Val + current_stage->src2Val;
+      stage_dest_val[STAGE_EX] = current_stage->src1Val + current_stage->src2Val;
       break;
     case CMD_SUB:
-      current_stage->src1Val = current_stage->src1Val - current_stage->src2Val;
+      stage_dest_val[STAGE_EX] = current_stage->src1Val - current_stage->src2Val;
       break;
     case CMD_SUBI:
-      current_stage->src1Val = current_stage->src1Val - current_stage->src2Val;
+      stage_dest_val[STAGE_EX] = current_stage->src1Val - current_stage->src2Val;
+      break;
+    default:
+      break;
+  }
+}
+
+void DoMemStage() {
+  PipeStageState* current_stage = &state.pipeStageState[STAGE_MEM];
+  switch(current_stage->cmd.opcode) {
+    case CMD_LOAD:
+      if (0 != SIM_MemDataRead(current_stage->src1Val + current_stage->src2Val, &stage_dest_val[STAGE_MEM])) {
+        last_read_mem_failed = true;
+      }
+      break;
+    case CMD_STORE:
+      SIM_MemDataWrite(stage_dest_val[STAGE_MEM] + current_stage->src2Val, current_stage->src1Val);
+      break;
+    default:
       break;
   }
 }
@@ -66,11 +94,11 @@ void DoWBStage() {
   if (state.pipeStageState[STAGE_WB].cmd.dst == 0) return;
   
   if (CMD_NOP < current_stage->cmd.opcode && CMD_SUBI >= current_stage->cmd.opcode) {
-    state.regFile[current_stage->cmd.dst] = current_stage->src1Val;
+    state.regFile[current_stage->cmd.dst] = stage_dest_val[STAGE_WB];
   }
   
   else if (CMD_LOAD == current_stage->cmd.opcode) {
-    state.regFile[current_stage->cmd.dst] = current_stage->src2Val;
+    state.regFile[current_stage->cmd.dst] = stage_dest_val[STAGE_WB];
 
     if (forwarding) {
       PipeStageState* ex_stage = &state.pipeStageState[STAGE_EX];
@@ -83,7 +111,6 @@ void DoWBStage() {
         ex_stage->src2Val = state.regFile[current_stage->cmd.dst];
       }
     }
-    
   }
 }
 
@@ -91,15 +118,20 @@ void DoWBStage() {
   This function is expected to update the core pipeline given a clock cycle event.
 */
 void SIM_CoreClkTick() {
-  
-  state.pc += 4;
-  
+    
   DoWBStage();
-  /* TODO Continue Execute Stages */
+  DoMemStage();
   DoEXStage();
   DoIDStage();
 
-  
+  if (last_read_mem_failed) {
+    reset_stage(STAGE_WB);
+    last_read_mem_failed = false;
+    return; //Please notice the function returns
+  }
+
+  state.pc += 4;
+
   if (forwarding) {
     
   }
@@ -108,29 +140,33 @@ void SIM_CoreClkTick() {
   }
 
   else { //Only Stalling
-    for (int i = STAGE_WB; i > STAGE_IF; i--) { 
+    for (int i = STAGE_WB; i >= STAGE_EX; i--) { 
       state.pipeStageState[i] = state.pipeStageState[i-1];
+      stage_dest_val[i] = stage_dest_val[i-1];
     }
     bool data_hazard_detected = false;
-    for (int i = STAGE_ID; i < SIM_PIPELINE_DEPTH; i ++){ 
+    for (int i = STAGE_EX; i < SIM_PIPELINE_DEPTH; i ++){ 
       if (state.pipeStageState[i].cmd.opcode > CMD_NOP && state.pipeStageState[i].cmd.opcode <= CMD_LOAD) {
         int stage_dest = state.pipeStageState[i].cmd.dst;
-        if (state.pipeStageState[STAGE_IF].cmd.src1 == stage_dest ||
-            (!state.pipeStageState[STAGE_IF].cmd.isSrc2Imm) 
-              && state.pipeStageState[STAGE_IF].cmd.src2 == stage_dest) {
+        if (state.pipeStageState[STAGE_ID].cmd.src1 == stage_dest ||
+            (!state.pipeStageState[STAGE_ID].cmd.isSrc2Imm) 
+              && state.pipeStageState[STAGE_ID].cmd.src2 == stage_dest) {
               data_hazard_detected = true;
               break;         
         }
       }
     }
     if (data_hazard_detected) {
-      reset_cmd(&state.pipeStageState[STAGE_ID].cmd); 
+      reset_stage(STAGE_EX);
     }
     else {
-      state.pipeStageState[STAGE_ID] = state.pipeStageState[STAGE_IF]; 
+      state.pipeStageState[STAGE_ID] = state.pipeStageState[STAGE_IF];
+      stage_dest_val[STAGE_ID] = 0;
       SIM_MemInstRead(state.pc, &state.pipeStageState[STAGE_IF].cmd);
+      stage_dest_val[STAGE_IF] = 0;
     }
   }
+  DoIDStage(); // For some reason the results expects ID stage to be done also here.
 }
 
 /*! SIM_CoreGetState: Return the current core (pipeline) internal state
